3c71c340a5c90d3e0de1d715bc6027664c5fec00
Morgan Phillips <winter2718@gmail.com>
2015-07-10 23:29:46 +0000

    Remove un-needed variable

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ve
 # USAGE: Run this file using `npm test` (must run from repository root)
 
-export influx_connectionString=${influx_connectionString-"https://localhost"}
-export webhook_secret="il-se-cache"
-
+export webhook_secret="c-etait-un-secret"
 mocha                               \
   test/taskcluster_github_test.js   \
   ;
