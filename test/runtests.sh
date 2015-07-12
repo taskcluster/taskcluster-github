5f34fc45ef6e65eeccef59a0df6afdc9f33470ac
Morgan Phillips <winter2718@gmail.com>
2015-07-12 07:59:07 +0000

    Remove environment variable export from runtests.sh

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ve
 # USAGE: Run this file using `npm test` (must run from repository root)
 
-export webhook_secret="c-etait-un-secret"
 mocha                               \
   test/taskcluster_github_test.js   \
   ;
