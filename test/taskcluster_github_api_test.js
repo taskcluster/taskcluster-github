463a2ed983ae18de4b9cdd452d8dd2a6253ba96f
Morgan Phillips <winter2718@gmail.com>
2015-07-13 01:47:09 +0000

    Improve test names.

--- test/taskcluster_github_api_test.js
+++ test/taskcluster_github_api_test.js
@@ -15,12 +15,12 @@ suite("TaskCluster-Github", () => {
   }
 
   // Good data: should all return 200 responses
-  statusTest('pullRequestOpen', 'webhook.pull_request.open.json', 204)
-  statusTest('pullRequestClose', 'webhook.pull_request.close.json', 204)
-  statusTest('push', 'webhook.push.json', 204)
+  statusTest('Pull Request Opened', 'webhook.pull_request.open.json', 204)
+  statusTest('Pull Request Closed', 'webhook.pull_request.close.json', 204)
+  statusTest('Push', 'webhook.push.json', 204)
 
   // Bad data: should all return 400 responses
-  statusTest('pushWithNoSecret', 'webhook.push.no_secret.json', 400)
-  statusTest('unknownEvent', 'webhook.unknown_event.json', 400)
-  statusTest('pushWithBadSecret', 'webhook.push.bad_secret.json', 403)
+  statusTest('Push without secret', 'webhook.push.no_secret.json', 400)
+  statusTest('Unknown Event', 'webhook.unknown_event.json', 400)
+  statusTest('Push with bad secret', 'webhook.push.bad_secret.json', 403)
 });
