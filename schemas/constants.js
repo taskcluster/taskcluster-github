bb0f624511651bcb0218921fc2188863602f13bd
Morgan Phillips <winter2718@gmail.com>
2015-07-18 02:11:12 +0000

    make schema constant names more specific.

--- schemas/constants.js
+++ schemas/constants.js
@@ -8,14 +8,14 @@
  * that can be reused.
  */
 module.exports = {
-  // Identifier pattern, min and max length, these limitations are applied to
-  // all common identifiers. It's not personal, it's just that without these
-  // limitation, the identifiers won't be useful as routing keys in RabbitMQ
+  // github-identifier pattern, min and max length, these limitations are applied to
+  // all common github-identifiers. It's not personal, it's just that without these
+  // limitation, the github-identifiers won't be useful as routing keys in RabbitMQ
   // topic exchanges. Specifically, the length limitation and the fact that
-  // identifiers can't contain dots `.` is critical.
-  "identifier-pattern":     "^([a-zA-Z0-9-_%]*)$",
-  "identifier-min-length":  1,
-  "identifier-max-length":  100,
+  // github-identifiers can't contain dots `.` is critical.
+  "github-identifier-pattern":     "^([a-zA-Z0-9-_%]*)$",
+  "github-identifier-min-length":  1,
+  "github-identifier-max-length":  100,
 
   // Message version numbers
   "message-version": {
