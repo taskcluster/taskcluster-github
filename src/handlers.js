75a09bcd45679f1f5d1a3e4b437912fc9fb4ac53
Dustin J. Mitchell <dustin@mozilla.com>
2017-02-07 18:46:33 +0000

    Fix tests to use fake github -- and they all pass!

--- src/handlers.js
+++ src/handlers.js
@@ -115,6 +115,18 @@ class Handlers {
       this.connection = undefined;
     }
   }
+
+  // Create a collection of tasks, centralized here to enable testing without creating tasks.
+  async createTasks({scopes, tasks}) {
+    debug('Creating queue...');
+    let queue = new taskcluster.Queue({
+      baseUrl: this.context.cfg.taskcluster.queueBaseUrl,
+      credentials: this.context.cfg.taskcluster.credentials,
+      authorizedScopes: scopes,
+    });
+    debug('Creating tasks...');
+    await Promise.all(tasks.map(t => queue.createTask(t.taskId, t.task)));
+  }
 }
 module.exports = Handlers;
 
@@ -258,14 +270,7 @@ async function jobHandler(message) {
     });
     if (graphConfig.tasks.length) {
       let taskGroupId = graphConfig.tasks[0].task.taskGroupId;
-      debug('Creating queue...');
-      let queue = new taskcluster.Queue({
-        baseUrl: context.cfg.taskcluster.queueBaseUrl,
-        credentials: context.cfg.taskcluster.credentials,
-        authorizedScopes: graphConfig.scopes,
-      });
-      debug('Creating tasks...');
-      await Promise.all(graphConfig.tasks.map(t => queue.createTask(t.taskId, t.task)));
+      await this.createTasks({scopes: graphConfig.scopes, tasks: graphConfig.tasks});
 
       // We used to comment on every commit, but setting the status
       // is a nicer thing to do instead. It contains all of the same
