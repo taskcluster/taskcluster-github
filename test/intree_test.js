6844147eebd449bc7e9574fe310e0ba6cb70ef5c
owlishDeveloper <bugzeeeeee@gmail.com>
2018-05-13 15:49:57 -0700

    Added a schema

--- test/intree_test.js
+++ test/intree_test.js
@@ -1,8 +1,9 @@
-suite('intree config', () => {
-  let fs = require('fs');
-  let assert = require('assert');
-  let _ = require('lodash');
-  let helper = require('./helper');
+let fs = require('fs');
+let assert = require('assert');
+let _ = require('lodash');
+let helper = require('./helper');
+
+suite('intree config, v0', () => {
 
   /**
    * Test github data, like one would see in a pulse message
@@ -75,7 +76,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Single Task Config',
-    configPath + 'taskcluster.single.yml',
+    configPath + 'taskcluster.single.v0.yml',
     {
       payload:    buildMessage(),
     },
@@ -86,7 +87,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config',
-    configPath + 'taskcluster.single.yml',
+    configPath + 'taskcluster.single.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push'}}),
     },
@@ -98,7 +99,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event (Push Task + Pull Task + Release Task)',
-    configPath + 'taskcluster.push_pull_release.yml',
+    configPath + 'taskcluster.push_pull_release.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push'}}),
     },
@@ -111,7 +112,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Pull Event (Push Task + Pull Task + Release Task)',
-    configPath + 'taskcluster.push_pull_release.yml',
+    configPath + 'taskcluster.push_pull_release.v0.yml',
     {
       payload:    buildMessage(),
     },
@@ -124,7 +125,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Limited (on branch)',
-    configPath + 'taskcluster.branchlimited.yml',
+    configPath + 'taskcluster.branchlimited.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'master'}}),
     },
@@ -137,7 +138,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Limited (off branch)',
-    configPath + 'taskcluster.branchlimited.yml',
+    configPath + 'taskcluster.branchlimited.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'foobar'}}),
     },
@@ -147,7 +148,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Excluded (on branch)',
-    configPath + 'taskcluster.exclude.yml',
+    configPath + 'taskcluster.exclude.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'foobar'}}),
     },
@@ -157,7 +158,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Pull Request Event, Single Task Config, Branch Excluded (on branch)',
-    configPath + 'taskcluster.pull_with_exclude.yml',
+    configPath + 'taskcluster.pull_with_exclude.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'pull_request.opened', 'event.base.repo.branch': 'master'}}),
     },
@@ -167,7 +168,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Exclude and Include errors',
-    configPath + 'taskcluster.exclude-error.yml',
+    configPath + 'taskcluster.exclude-error.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'master'}}),
     },
@@ -189,7 +190,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Release Event, Single Task Config',
-    configPath + 'taskcluster.release_single.yml',
+    configPath + 'taskcluster.release_single.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'release'}}),
     },
@@ -201,7 +202,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Release Event (Push Task + Pull Task + Release Task)',
-    configPath + 'taskcluster.push_pull_release.yml',
+    configPath + 'taskcluster.push_pull_release.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'release'}}),
     },
@@ -213,7 +214,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'No extra or extra.github generates an empty config',
-    configPath + 'taskcluster.non-github.yml',
+    configPath + 'taskcluster.non-github.v0.yml',
     {
       payload:    buildMessage({details: {'event.type': 'release'}}),
     },
@@ -222,7 +223,7 @@ suite('intree config', () => {
 
   buildConfigTest(
     'Unicode branch names are not allowed',
-    configPath + 'taskcluster.single.yml',
+    configPath + 'taskcluster.single.v0.yml',
     {
       payload: buildMessage({details: {'event.base.repo.branch': '🌱', 'event.type': 'push'}}),
     },
@@ -252,5 +253,88 @@ suite('intree config', () => {
       'tasks[0].task.extra.github.events': ['tag'],
       'metadata.owner': 'test@test.com',
       scopes: ['assume:repo:github.com/testorg/testrepo:tag:v1.0.2'],
+    });
+});
+
+suite('intree config, v1', () => {
+
+  /**
+   * Test github data, like one would see in a pulse message
+   * after a pull request
+   **/
+  function buildMessage(params) {
+    let defaultMessage = {
+      organization: 'testorg',
+      repository:   'testrepo',
+      details: {
+        'event.pullNumber': 'eventData.number',
+        'event.type': 'pull_request.opened',
+        'event.base.user.login': 'eventData.pull_request.base.user.login',
+        'event.base.repo.name': 'eventData.pull_request.base.repo.name',
+        'event.base.repo.url': 'eventData.pull_request.base.repo.clone_url',
+        'event.base.repo.branch': 'default_branch',
+        'event.base.sha': 'eventData.pull_request.base.sha',
+        'event.base.ref': 'eventData.pull_request.base.ref',
+        'event.head.user.login': 'eventData.pull_request.head.user.login',
+        'event.head.repo.name': 'eventData.pull_request.head.repo.name',
+        'event.head.repo.url': 'eventData.pull_request.head.repo.clone_url',
+        'event.head.repo.branch': 'eventData.pull_request.head.default_branch',
+        'event.head.sha': 'eventData.pull_request.head.sha',
+        'event.head.ref': 'eventData.pull_request.head.ref',
+        'event.head.user.email': 'test@test.com',
+      },
+    };
+    return _.merge(defaultMessage, params);
+  };
+  
+  /**
+   * Make sure that data merges properly when building configs
+   * testName:    '', A label for the current test case
+   * configPath:  '', Path to a taskclusterConfig file
+   * params:      {
+   *                payload:    {}, WebHook message payload
+   *                validator:  {}, A taskcluster.base validator
+   *              }
+   * count:       number of tasks to expect
+   * expected:    {}, keys=>values expected to exist in the compiled config
+   * shouldError: if you want intree to throw an exception, set this to true
+   **/
+  let buildConfigTest = function(testName, configPath, params, expected, count=-1, shouldError=false) {
+    test(testName, async () => {
+      params.config = fs.readFileSync(configPath);
+      params.schema = fs.readFileSync('test/data/schemas/v1.json');
+      params.validator = helper.validator;
+      let config;
+      try {
+        config = helper.intree(params);
+      } catch (e) {
+        if (shouldError) {
+          return;
+        }
+        throw e;
+      }
+      if (shouldError) {
+        throw new Error('This intree call should have failed!');
+      }
+      if (count > 0) {
+        assert.equal(config.tasks.length, count);
+      }
+      for (let key of Object.keys(expected)) {
+        assert.deepEqual(_.get(config, key), expected[key]);
+      }
+    });
+  };
+
+  let configPath = 'test/data/configs/';
+
+  buildConfigTest(
+    'Single Task Config',
+    configPath + 'taskcluster.single.v1.yml',
+    {
+      payload:    buildMessage(),
+    },
+    {
+      tasks: [], // The github event doesn't match, so no tasks are created
+      'metadata.owner': 'test@test.com',
     });
 });
