a0ce9062197d9876563fbfb27debac29161cdf17
owlishDeveloper <bugzeeeeee@gmail.com>
2018-05-21 19:20:45 -0700

    Made old tests pass after rebase

--- test/intree_test.js
+++ test/intree_test.js
@@ -1,9 +1,8 @@
-let fs = require('fs');
-let assert = require('assert');
-let _ = require('lodash');
-let helper = require('./helper');
-
-suite('intree config, v0', () => {
+suite('intree config', () => {
+  let fs = require('fs');
+  let assert = require('assert');
+  let _ = require('lodash');
+  let helper = require('./helper');
 
   /**
    * Test github data, like one would see in a pulse message
@@ -125,7 +124,7 @@ suite('intree config, v0', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Limited (on branch)',
-    configPath + 'taskcluster.branchlimited.v0.yml',
+    configPath + 'taskcluster.branchlimited.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'master'}}),
     },
@@ -138,7 +137,7 @@ suite('intree config, v0', () => {
 
   buildConfigTest(
     'Push Event, Single Task Config, Branch Limited (off branch)',
-    configPath + 'taskcluster.branchlimited.v0.yml',
+    configPath + 'taskcluster.branchlimited.yml',
     {
       payload:    buildMessage({details: {'event.type': 'push', 'event.base.repo.branch': 'foobar'}}),
     },
@@ -253,88 +252,5 @@ suite('intree config, v0', () => {
       'tasks[0].task.extra.github.events': ['tag'],
       'metadata.owner': 'test@test.com',
       scopes: ['assume:repo:github.com/testorg/testrepo:tag:v1.0.2'],
-    });
-});
-
-suite('intree config, v1', () => {
-
-  /**
-   * Test github data, like one would see in a pulse message
-   * after a pull request
-   **/
-  function buildMessage(params) {
-    let defaultMessage = {
-      organization: 'testorg',
-      repository:   'testrepo',
-      details: {
-        'event.pullNumber': 'eventData.number',
-        'event.type': 'pull_request.opened',
-        'event.base.user.login': 'eventData.pull_request.base.user.login',
-        'event.base.repo.name': 'eventData.pull_request.base.repo.name',
-        'event.base.repo.url': 'eventData.pull_request.base.repo.clone_url',
-        'event.base.repo.branch': 'default_branch',
-        'event.base.sha': 'eventData.pull_request.base.sha',
-        'event.base.ref': 'eventData.pull_request.base.ref',
-        'event.head.user.login': 'eventData.pull_request.head.user.login',
-        'event.head.repo.name': 'eventData.pull_request.head.repo.name',
-        'event.head.repo.url': 'eventData.pull_request.head.repo.clone_url',
-        'event.head.repo.branch': 'eventData.pull_request.head.default_branch',
-        'event.head.sha': 'eventData.pull_request.head.sha',
-        'event.head.ref': 'eventData.pull_request.head.ref',
-        'event.head.user.email': 'test@test.com',
-      },
-    };
-    return _.merge(defaultMessage, params);
-  };
-  
-  /**
-   * Make sure that data merges properly when building configs
-   * testName:    '', A label for the current test case
-   * configPath:  '', Path to a taskclusterConfig file
-   * params:      {
-   *                payload:    {}, WebHook message payload
-   *                validator:  {}, A taskcluster.base validator
-   *              }
-   * count:       number of tasks to expect
-   * expected:    {}, keys=>values expected to exist in the compiled config
-   * shouldError: if you want intree to throw an exception, set this to true
-   **/
-  let buildConfigTest = function(testName, configPath, params, expected, count=-1, shouldError=false) {
-    test(testName, async () => {
-      params.config = fs.readFileSync(configPath);
-      params.schema = 'http://schemas.taskcluster.net/queue/v1/create-task-request.json#';
-      params.validator = helper.validator;
-      let config;
-      try {
-        config = helper.intree(params);
-      } catch (e) {
-        if (shouldError) {
-          return;
-        }
-        throw e;
-      }
-      if (shouldError) {
-        throw new Error('This intree call should have failed!');
-      }
-      if (count > 0) {
-        assert.equal(config.tasks.length, count);
-      }
-      for (let key of Object.keys(expected)) {
-        assert.deepEqual(_.get(config, key), expected[key]);
-      }
-    });
-  };
-
-  let configPath = 'test/data/configs/';
-
-  buildConfigTest(
-    'Single Task Config',
-    configPath + 'taskcluster.single.v1.yml',
-    {
-      payload:    buildMessage(),
-    },
-    {
-      tasks: [], // The github event doesn't match, so no tasks are created
-      'metadata.owner': 'test@test.com',
     });
 });
