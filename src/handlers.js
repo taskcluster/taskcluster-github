60e2729c500b378faed033965654002dd79ce822
owlishDeveloper <bugzeeeeee@gmail.com>
2019-01-03 09:10:59 -0800

    Change binding for task-group-resolved event

--- src/handlers.js
+++ src/handlers.js
@@ -107,6 +107,7 @@ class Handlers {
       githubEvents.release(),
     ];
 
+    const schedulerId = this.context.cfg.taskcluster.schedulerId;
     const queueEvents = new taskcluster.QueueEvents({rootUrl: this.rootUrl});
 
     const statusBindings = [
@@ -122,7 +123,7 @@ class Handlers {
     const deprecatedResultStatusBindings = [
       queueEvents.taskFailed(`route.${this.context.cfg.app.statusTaskRoute}`),
       queueEvents.taskException(`route.${this.context.cfg.app.statusTaskRoute}`),
-      queueEvents.taskGroupResolved(`route.${this.context.cfg.app.statusTaskRoute}`),
+      queueEvents.taskGroupResolved({schedulerId}),
     ];
 
     // Listen for taskGroupCreationRequested event to create initial status on github
